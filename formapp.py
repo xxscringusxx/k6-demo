3547c6183b4761493c04b281fe0b686cff67aa22
Jahan Khan <jahankhan2003@gmail.com>
2023-11-11 18:07:41 -0600

    added module

--- formapp.py
+++ formapp.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request
-from src.entrymethods import add_entry_to_file, create_entry, read_existing_entries
+from src import entrymethods 
 
 app = Flask(__name__)
 user_info_file = 'user_info.txt'
-unique_entries = read_existing_entries(user_info_file)
+unique_entries = entrymethods.read_existing_entries(user_info_file)
 
 @app.route('/')
 def index():
@@ -11,13 +11,13 @@ def index():
 
 @app.route('/save', methods=['POST'])
 def save():
-    entry = create_entry(request)
+    entry = entrymethods.create_entry(request)
 
     if entry in unique_entries:
         return 'Information already exists!'
 
     unique_entries.add(entry)
-    add_entry_to_file(user_info_file, entry)
+    entrymethods.add_entry_to_file(user_info_file, entry)
 
     return 'Information saved successfully!'
 
